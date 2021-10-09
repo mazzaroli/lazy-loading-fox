3bc5bd4c8930d4bd023a0ea1ecb57784b30bbc55
carlos <carlitosmazaroli@gmail.com>
2021-10-09 17:59:10 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,63 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registerImage } from "./lazy";
 
-console.log('Happy hacking :)')
+/*
+1. HTML (imagenes)
+2. imagenes html -> js
+2.1 imagenes aleatorias
+3. eventos -> dom -> agregar imagenes
+4. intersection Observer
+5. Aplicar lazy loading a las imagenes
+*/
+
+const minimum = 1;
+const maximum = 122;
+const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum;
+
+
+// Crear (1) imagen
+const createImageNode = () => {
+    const container = document.createElement('div') 
+    container.className = 'p-4'
+
+    const imagen = document.createElement('img');
+    imagen.className = 'mx-auto';
+    imagen.width = '320';
+    imagen.dataset.src = `https://randomfox.ca/images/${random()}.jpg`; //to do
+
+
+    const imageWrapper = document.createElement("div");
+    imageWrapper.className = "bg-gray-300";
+    imageWrapper.style.minHeight = "100px";
+    imageWrapper.style.display = "inline-block";
+
+    imageWrapper.appendChild(imagen);
+    container.appendChild(imageWrapper);
+
+    return container;
+};
+
+const nuevaImagen = createImageNode();
+const mountNode = document.getElementById('images')
+
+// Agregar al contenedor #imagen
+const addButton = document.getElementById('add');
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.append(newImage);
+    registerImage(newImage);
+};
+
+addButton.addEventListener('click', addImage);
+// mountNode.append(nuevaImagen);
+
+
+// Eliminar imagenes del contenedor
+const cleanButton = document.getElementById('clean')
+
+const cleanImages = () => {
+    [...mountNode.childNodes].forEach(child => {
+        child.remove();
+    }) 
+}
+
+cleanButton.addEventListener("click", cleanImages)
